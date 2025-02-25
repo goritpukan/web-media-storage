4cd4253a43e0b1bfcb0afb3493fade57399dd029
Vadym <vadimchunihin.v@gmail.com>
2025-02-25 15:23:14 +0100

    add swagger and automapper, configure some root services in nest.js project, fix double auth folder

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { AutomapperModule } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.${process.env.NODE_ENV}.env`, '.env'],
+    }),
+    AutomapperModule.forRoot({
+      strategyInitializer: classes(),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>('PORT');
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Web Storage Api')
+    .setDescription('Web storage API swagger documentation')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, documentFactory);
+
+  await app.listen(port);
 }
+
 bootstrap();
