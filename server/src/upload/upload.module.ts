aac62822d26467ca56e0eebb388c5bfb0792ec23
Vadym <vadimchunihin.v@gmail.com>
2025-03-31 19:57:17 +0200

    deleted some unused code and dependencies

--- server/src/upload/upload.module.ts
+++ server/src/upload/upload.module.ts
@@ -1,31 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
-import { ConfigService } from '@nestjs/config';
-import * as multerS3 from 'multer-s3';
-import { S3Client } from '@aws-sdk/client-s3';
 import { UploadService } from './upload.service';
 
 @Module({
-  imports: [
-    // MulterModule.registerAsync({
-    //   inject: [ConfigService],
-    //   useFactory: (configService: ConfigService) => ({
-    //     storage: multerS3({
-    //       s3: new S3Client({
-    //         region: configService.get<string>('S3_STORAGE_REGION'),
-    //         endpoint: configService.get<string>('S3_STORAGE_URL'),
-    //         credentials: {
-    //           accessKeyId: configService.get('S3_ACCESS_KEY'),
-    //           secretAccessKey: configService.get('S3_SECRET_ACCESS_KEY'),
-    //         },
-    //         forcePathStyle: true,
-    //       }),
-    //       bucket: configService.get<string>('S3_BUCKET_NAME'),
-    //
-    //     })
-    //   })
-    // })
-  ],
+  imports: [],
   providers: [UploadService],
   exports: [UploadService],
 })
