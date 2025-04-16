70646dad88511fbe9ff6c1733c165a9db5c92566
vadym <vadimchunihin.v@gmail.com>
2025-04-16 22:12:34 +0200

    added react-query to the project, created Providers component

---
+++ client/src/components/providers/Providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter';
+
+interface ProvidersProps {
+    children: React.ReactNode;
+}
+
+export default function Providers({children}: ProvidersProps) {
+    const [queryClient] = React.useState(() => new QueryClient());
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AppRouterCacheProvider>
+                {children}
+            </AppRouterCacheProvider>
+        </QueryClientProvider>
+    );
+}
